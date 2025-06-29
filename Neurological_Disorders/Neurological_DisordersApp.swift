2341b277973775321b2c800cbe0f7d886c2424a4
Cong Le <longchike@gmail.com>
2025-06-29 09:13:50 -0700

    The setup

---
+++ Neurological_Disorders/Neurological_DisordersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neurological_DisordersApp.swift
+//  Neurological_Disorders
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Neurological_DisordersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Neurological_DisordersUITests/Neurological_DisordersUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Neurological_DisordersUITestsLaunchTests.swift
+//  Neurological_DisordersUITests
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import XCTest
+
+final class Neurological_DisordersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
